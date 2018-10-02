0985cfd5ef0b6b09daddb6f8d350acbc67703a3d
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-02 12:00:30 -0400

    Updated Devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'welcome/home'
-  get 'users/show', to: 'users#show'
-  root 'welcome#home'
-  devise_for :users
+  #devise_for :users
+
+  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  root to: 'welcome#home'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+    # devise_for :users, controllers: {
+    #   sessions: 'users/sessions',
+    #   registrations: 'users/registrations',
+    #   passwords: 'users/passwords'
+    # }
+  #, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  
+
+  #
 end
