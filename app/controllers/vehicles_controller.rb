46b884b0c997e32e4ed29273128aef5997e13fb4
Stephen Patterson <swpat23@gmail.com>
2018-10-02 09:32:20 -0400

    added color to vehicle

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -9,12 +9,7 @@ class VehiclesController < ApplicationController
   end
  
   def new
-    # if current_user
       @vehicle = Vehicle.new
-    # else
-    #   flash[:notice] ="You must be logged to add a Vehicle."
-    #   redirect_to new_session_path
-    # end
   end
 
   def create
@@ -30,13 +25,8 @@ class VehiclesController < ApplicationController
 
   def destroy 
     @vehicle = Vehicle.find(params[:id])
-    if current_user.id == @vehicle.user_id
       @vehicle.destroy
       redirect_to vehicles_path
-    else
-      flash[:notice] = 'Only vehicle creator can delete.'
-      redirect_to @vehicle
-    end
   end
  
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_120704) do
+ActiveRecord::Schema.define(version: 2018_10_02_132058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_120704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "color"
   end
 
 end
