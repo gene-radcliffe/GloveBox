2454980d58d62b35d9c2808e912f089f69602a66
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-03 10:03:25 -0400

    finished mailer, started on user profile, and routed the main page to vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   #devise_for :users
 
   get 'welcome/home', to: 'welcome#home'
-  root to: 'welcome#home'
+  #root to: 'welcome#home'
   devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   devise_scope :user do
-    #root to: "devise/sessions#new"
+    root to: "devise/sessions#new"
   end
   
   
