07ea2e5945043bb0e2acf084fd759af7f9b21526
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-02 23:39:40 -0400

    Configured Email Server, Improved the routing after login, and started working on User profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+
   def after_sign_in_path_for(resource)
-    root_path
+    if user_signed_in?
+      root_path
+    end
   end
+
   def after_sign_out_path_for(resource_or_scope)
-    user_session_path
+    new_user_session_path
   end
+
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :email])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_10_02_022926) do
-
+ActiveRecord::Schema.define(version: 2018_10_02_132058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
