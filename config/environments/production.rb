fde84fd5ff5bd60059cb17a8b58ad892a64e3e22
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-06 13:12:09 -0400

    saving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
+    :user_name            => ENV['gmail_user_name'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
    :enable_starttls_auto => true
