7957e4b32356f4d733b088e212854947b0206920
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-08 08:44:17 -0400

    File uploads but blob is not saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
     def show
-       
+        byebug
+        
+        @user= current_user
         @vehicles = current_user.vehicles
     end
     
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_user_name'],
+    :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
    :enable_starttls_auto => true
