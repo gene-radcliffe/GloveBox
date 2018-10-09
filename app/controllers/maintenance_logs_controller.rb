4068304cd7d599cd060953c8e16cc3fc9ebfc7d4
josephrfarmer <josephr.farmer24@gmail.com>
2018-10-09 17:07:04 -0400

    working through saving maintenance log and actions

--- app/controllers/maintenance_logs_controller.rb
+++ app/controllers/maintenance_logs_controller.rb
@@ -19,14 +19,67 @@ class MaintenanceLogsController < ApplicationController
   end  
 
   def create 
+    @vehicle = Vehicle.find(params['vehicle_id'])
+    @mlog = @vehicle.maintenance_logs.new
+    actions = params[:selected_actions].split(",")
+    actions.map do |t|
+      @mlog.maintenance_actions.build(type: t)
       byebug
+      whitelisted_params = self.send("#{t.downcase}_params") 
+    end  
+      
 
   end  
 
 
   private 
   def maintenance_log_params
-    params.require(:maintenance_log).permit(:images, :vehicle_id)
- end
+    params.require(:maintenance_log)
+  end
+  
+  def oilchange_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :oil_type, :service_date, :cost, :notes)
+  end 
+
+  def brake_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :brake_location, :service_date, :cost, :notes)
+  end 
+
+  def rotor_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :rotor_location, :service_date, :cost, :notes)
+  end 
+
+  def TireRotation_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
+
+  def TireReplacement_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :tire_location, :service_date, :cost, :notes)
+  end 
+
+  def Alignment_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
+
+  def PowerSteeringFluid_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
+
+  def AntiFreeze_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
+
+  def TransmissionFluid_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
+
+
+  def BrakeFluid_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
+
+  def Miscellaneous_params 
+    maintenance_log_params[:oilchange].permit(:mileage, :service_date, :cost, :notes)
+  end 
 
 end
