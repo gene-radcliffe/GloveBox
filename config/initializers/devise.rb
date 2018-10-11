e3b8289113130e0062d849b6ed3e9a37fb5832ab
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-11 15:49:00 -0400

    configured devise mailer default url

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'generadcliffe.momentum@gmail.com'
+  config.mailer_sender = 'app110448060@heroku.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
