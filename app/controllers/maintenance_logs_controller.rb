085fbcf72e48f1a6f1316ad4ae283b0e5848f1c9
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-12 10:54:42 -0400

    saving

--- app/controllers/maintenance_logs_controller.rb
+++ app/controllers/maintenance_logs_controller.rb
@@ -8,7 +8,7 @@ class MaintenanceLogsController < ApplicationController
   end  
 
   def create 
-    bye
+    byebug
     @vehicle = Vehicle.find(params['vehicle_id'])
     @mlog = @vehicle.maintenance_logs.create(:image => params[:maintenance_log][:image])
     
