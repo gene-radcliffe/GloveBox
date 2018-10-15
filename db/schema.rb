b26ec2c6665da314a3c9595d90386ceaba34f6c8
gene-radcliffe <g_radcliffe@hotmail.com>
2018-10-15 15:33:57 -0400

    saving

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_200637) do
+ActiveRecord::Schema.define(version: 2018_10_15_144526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2018_10_14_200637) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "converted_receipts", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "maintenance_log_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
@@ -118,7 +126,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_200637) do
     t.string "tire_psi"
     t.string "registration"
     t.string "title"
-    t.date "inspection_date"
+    t.string "inspection_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
